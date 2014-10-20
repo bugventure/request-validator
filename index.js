c8b4861a12dee1c922e938a71fb7e3104d81067d
Veli Pehlivanov <veli.pehlivanov@telerik.com>
2014-10-20 17:54:27 +0300

    add error reporting unit tests

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = require('./lib/validator.js');
 
 // TODO:
-// 1. Data collection from request params
-// 2. Error messages and reporting (incl. nested errors and strings)
-// 3. Validator extensibility
-// 4. Documentation (incl. TOC)
+// 1. Validator extensibility
+// 2. Documentation (incl. TOC)
--- test/error.js
+++ test/error.js
@@ -81,4 +81,139 @@ describe('error', function () {
             });
         });
     });
+
+    it('required message', function () {
+        try {
+            validator({
+                type: 'string',
+                required: true
+            }).validate();
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.message, 'anonymous: required');
+        }
+
+        try {
+            validator({
+                name: 'field1',
+                type: 'string',
+                required: true
+            }).validate();
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.message, 'field1: required');
+        }
+    });
+
+    it('invalid message', function () {
+        try {
+            validator({
+                type: 'string'
+            }).validate(123);
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.key, undefined);
+            assert.strictEqual(e.message, 'anonymous: invalid');
+        }
+
+        try {
+            validator({
+                name: 'field1',
+                type: 'string'
+            }).validate(123);
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.key, 'field1');
+            assert.strictEqual(e.message, 'field1: invalid');
+        }
+    });
+
+    it('custom invalid message', function () {
+        try {
+            validator({
+                type: 'string',
+                required: true,
+                message: 'custom message'
+            }).validate(123);
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.key, undefined);
+            assert.strictEqual(e.message, 'anonymous: custom message');
+        }
+
+        try {
+            validator({
+                name: 'field1',
+                type: 'string',
+                required: true,
+                message: 'custom message'
+            }).validate(123);
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.key, 'field1');
+            assert.strictEqual(e.message, 'field1: custom message');
+        }
+
+        try {
+            validator({
+                name: 'field1',
+                type: 'string',
+                required: true,
+                message: 'custom message'
+            }).validate();
+
+            assert.fail();
+        }
+        catch (e) {
+            // does not apply for required fields
+            assert.strictEqual(e.key, 'field1');
+            assert.strictEqual(e.message, 'field1: required');
+        }
+    });
+
+    it('nested messages', function () {
+        try {
+            validator({
+                type: 'object',
+                properties: [
+                    {
+                        name: 'a',
+                        type: 'string'
+                    },
+                    {
+                        name: 'b',
+                        type: 'string'
+                    }
+                ]
+            }).validate({ a: 1, b: 2 });
+
+            assert.fail();
+        }
+        catch (e) {
+            assert.strictEqual(e.key, undefined);
+            assert.strictEqual(e.message, 'anonymous: invalid');
+            assert(e.errors instanceof Array);
+            assert.strictEqual(e.errors.length, 2);
+
+            assert(e.errors[0] instanceof Error);
+            assert.strictEqual(e.errors[0].key, 'a');
+            assert.strictEqual(e.errors[0].message, 'a: invalid');
+
+            assert(e.errors[1] instanceof Error);
+            assert.strictEqual(e.errors[1].key, 'b');
+            assert.strictEqual(e.errors[1].message, 'b: invalid');
+        }
+    });
 });
