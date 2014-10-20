7b0f29cc57446dd1a5ed16ea1993e95e7b5e4d81
Veli Pehlivanov <veli.pehlivanov@telerik.com>
2014-10-20 22:55:10 +0300

    refactor validator for extensibility

---
+++ test/extensibility.js
@@ -0,0 +1,33 @@
+/* global describe, it */
+'use strict';
+
+var assert = require('assert'),
+    validator = require('../index.js');
+
+describe('extensibility', function () {
+    it('can create multiple instances', function () {
+        var validator2 = validator.create();
+
+        assert(validator2 instanceof Function);
+        assert.notStrictEqual(validator2, validator);
+    });
+
+    it('copy has all the extensions', function () {
+        var validator2 = validator.create();
+
+        assert.strictEqual(validator2.type, validator.type);
+        assert.strictEqual(validator2.equal, validator.equal);
+        assert.strictEqual(validator2.chain, validator.chain);
+        assert.strictEqual(validator2.create, validator.create);
+    });
+
+    it('copy has copies of extensions with state', function () {
+        var validator2 = validator.create();
+
+        assert.strictEqual(JSON.stringify(validator2.strings), JSON.stringify(validator.strings));
+        assert.notStrictEqual(validator2.strings, validator.strings);
+
+        assert.strictEqual(typeof validator2.validateSchema, typeof validator.validateSchema);
+        assert.notStrictEqual(validator2.validateSchema, validator.validateSchema);
+    });
+});
